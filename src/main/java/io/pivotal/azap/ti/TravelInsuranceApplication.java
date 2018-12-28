713e2a86c02b319a29d9328aacf7522c1e5bf35f
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2018-12-28 11:36:59 +0800

    init

---
+++ src/main/java/io/pivotal/azap/ti/TravelInsuranceApplication.java
@@ -0,0 +1,14 @@
+package io.pivotal.azap.ti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelInsuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelInsuranceApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/io/pivotal/azap/ti/TravelInsuranceApplicationTests.java
@@ -0,0 +1,17 @@
+package io.pivotal.azap.ti;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TravelInsuranceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
