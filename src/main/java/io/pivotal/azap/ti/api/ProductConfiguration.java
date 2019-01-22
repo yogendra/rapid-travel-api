de38c6632aa3828444fe2eb5cc0080aedc01a263
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2019-01-22 12:10:26 +0700

    set product prices

--- src/main/java/io/pivotal/azap/ti/api/ProductConfiguration.java
+++ src/main/java/io/pivotal/azap/ti/api/ProductConfiguration.java
@@ -5,6 +5,7 @@ import static io.pivotal.azap.ti.PremiumFrequency.ONE_TIME;
 import static io.pivotal.azap.ti.api.Product.ProductType.SPORT_SKI;
 import static io.pivotal.azap.ti.api.Product.ProductType.TRAVEL;
 
+import io.pivotal.azap.ti.api.Product.PolicyParameter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -38,6 +39,20 @@ public class ProductConfiguration {
         .premiumType(ONE_TIME)
         .premium(USD(250.00))
         .coverage(USD(10000.00))
+        .build();
+  }
+
+  @Bean
+  public Product hourlySkiProduct() {
+    return Product.builder()
+        .code("HSKI001")
+        .name("Hourly Ski Policy ")
+        .description("Gives you coverage by hour")
+        .type(SPORT_SKI)
+        .premiumType(ONE_TIME)
+        .premium(USD(10.00))
+        .coverage(USD(10000.00))
+        .parameter(PolicyParameter.requiredNumber("Hours", "No. of Hours"))
         .build();
   }
 }
