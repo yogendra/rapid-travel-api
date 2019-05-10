67601e37d2abe3e2b085a83f51bb8c588eae13e4
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2019-05-10 13:23:31 +0800

    Added new plan for hourly

--- src/main/java/io/pivotal/azap/ti/api/ProductConfiguration.java
+++ src/main/java/io/pivotal/azap/ti/api/ProductConfiguration.java
@@ -4,6 +4,7 @@ import static io.pivotal.azap.ti.Amount.USD;
 import static io.pivotal.azap.ti.PremiumFrequency.ONE_TIME;
 import static io.pivotal.azap.ti.api.Product.ProductType.SPORT_SKI;
 
+import io.pivotal.azap.ti.api.Product.PolicyParameter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,6 +23,19 @@ public class ProductConfiguration {
         .premiumType(ONE_TIME)
         .premium(USD(250.00))
         .coverage(USD(10000.00))
+        .build();
+  }
+  @Bean
+  public Product hourlySkiProduct() {
+    return Product.builder()
+        .code("HSKI001")
+        .name("Hourly Ski Policy ")
+        .description("Gives you coverage by hour")
+        .type(SPORT_SKI)
+        .premiumType(ONE_TIME)
+        .premium(USD(10.00))
+        .coverage(USD(100.00))
+        .parameter(PolicyParameter.requiredNumber("Hours", "No. of Hours"))
         .build();
   }
 
